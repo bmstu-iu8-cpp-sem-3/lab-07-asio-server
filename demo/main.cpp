8c82a052a816f47c3b215ef40c82c13032bade9e
ZeroSevn <artem.malenko@yandex.ru>
2021-01-19 21:55:44 +0300

    Fix

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,8 @@
 #include <header.hpp>
 
 int main() {
-
+    boost::thread_group threads;
+    threads.create_thread(accept_thread);
+    threads.create_thread(handle_clients_thread);
+    threads.join_all();
 }
